c134fdb5e3b5b6d3410da44201c0309bd82784b1
wfadam <wfadamweb@gmail.com>
2015-07-09 17:11:53 +0800

    Update accurev.go
    improved interactive display logic

--- accurev.go
+++ accurev.go
@@ -6,31 +6,25 @@ import (
 	"os/exec"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var sel []string
+var str []byte
 
 func main() {
-	// disable input buffering
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	// do not display entered characters on the screen
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 
-	allStreams := []string{
-		"T73_1Znm_128gb_ABL_eX3_2P_SDSIP_768_16D",
-		"T73_1Ynm_64gb_ABL_eX3_1P_TSOP_768_1D",
-		"T73_1Znm_64gb_ABL_eX2_4P_SSD-BGA_768_8D",
-	}
+	allStreams := queryStreams()
 
 	done := make(chan bool)
 	ch := make(chan string)
 	go inputProName(ch, done)
-	go search(ch, allStreams )
+	go search(ch, allStreams)
 
 	<-done
 
 	baseStream := setStream()
-	fmt.Printf( "\n%s\n", baseStream )
+	fmt.Printf("\n%s\n", baseStream)
 
 }
 
@@ -38,11 +32,13 @@ func getStream(ch <-chan string) {
 	fmt.Printf("\nBase Stream is %s\n", <-ch)
 }
 
-func search(ch <-chan string, allStreams []string ) {
+func search(ch <-chan string, allStreams []string) {
 	caseIgnore := "(?i)"
 	for {
 		pat := <-ch
 		if len(pat) == 0 {
+			sel = []string{}
+			disp(str, sel)
 			continue
 		}
 
@@ -57,31 +53,38 @@ func search(ch <-chan string, allStreams []string ) {
 				sel = append(sel, s)
 			}
 		}
-
-		fmt.Println()
-		for i, s := range sel {
-			fmt.Printf("\t%d : %s\n", i, s)
-		}
-		fmt.Println("Press <Enter> to proceed")
+		disp(str, sel)
 	}
 }
 
+func disp(str []byte, sel []string) {
+	fmt.Print("\n\n\n")
+	for i, s := range sel {
+		fmt.Printf("\t%d : %s\n", i, s)
+	}
+	fmt.Printf("\nSearch Base Stream : %s", string(str))
+}
+
 func setStream() string {
-	if len( sel ) == 0 {
-		fmt.Printf("\nNothing is selected, so quit\n" )
+	if len(sel) == 0 {
+		fmt.Printf("\nNothing is selected, so quit\n")
 		os.Exit(1)
+	}
+	if len(sel) == 1 {
+		return sel[0]
 	}
 
 	exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+	//resetTTY()
 
 tryAgain:
-	fmt.Print("\nSelect Stream : ")
+	fmt.Print("\nChoose Stream# : ")
 	var b []byte = make([]byte, 1)
 
 	os.Stdin.Read(b)
-	i,err := strconv.ParseInt( string( b[0] ), 10, 8 )
-	if err != nil {
-		fmt.Printf( "\nInput an integer from 0 to %d\n", len(sel)-1)
+	i, err := strconv.ParseInt(string(b[0]), 10, 8)
+	if err != nil || int(i) >= len(sel) {
+		fmt.Printf("\nInput an integer from 0 to %d\n", len(sel)-1)
 		goto tryAgain
 	}
 
@@ -90,11 +93,15 @@ tryAgain:
 }
 
 func inputProName(ch chan<- string, done chan<- bool) {
-	str := []byte{}
+	setTTY()
+
+	str = []byte{}
 	var b []byte = make([]byte, 1)
+	
+	fmt.Printf("Search Backing Stream : %s", string(str))
+
 outer:
 	for {
-		fmt.Printf("\nSearch Stream : %s", string(str))
 		os.Stdin.Read(b)
 		switch b[0] {
 		case 0x7F:
@@ -110,3 +117,43 @@ outer:
 		ch <- string(str)
 	}
 }
+
+func queryStreams() []string {
+	fmt.Println("Connecting to Accurev Server ...")
+	cmd := "accurev"
+	args := []string{"show", "streams", "-d", "-p", "MT_Production_Test_Programs"}
+	o, e := exec.Command(cmd, args...).Output()
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
+	}
+
+	sms := []string{}
+	for _, b := range strings.Split(string(o), "\n") {
+		ln := strings.Split(strings.TrimRight(b, " "), " ")
+		if len(ln) > 0 && strings.EqualFold( "Y", ln[len(ln)-1]) { // true for Dynamic Stream
+			sms = append(sms, ln[0])
+		}
+	}
+	fmt.Printf("%d Dynamic Streams are queried\n", len(sms))
+	return sms
+
+	//return []string{
+	//	"T73_1Znm_128gb_ABL_eX3_2P_SDSIP_768_16D",
+	//	"T73_1Ynm_64gb_ABL_eX3_1P_TSOP_768_1D",
+	//	"T73_1Znm_64gb_ABL_eX2_4P_SSD-BGA_768_8D",
+	//}
+}
+
+func setTTY() {
+	// disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+}
+
+func resetTTY() {
+	exec.Command("stty", "-F", "/dev/tty", "sane").Run()
+}
+
+
